b21b0e905ce9763e922258a488ce6d257ea1a10e
JuanJaramilloSalazar <salazarjdj@gmail.com>
2022-09-19 19:51:50 -0500

    cambios

--- usuariosApp/models/medico.py
+++ usuariosApp/models/medico.py
@@ -3,10 +3,10 @@ from .user import User
 
 class Medico(models.Model):
     id = models.AutoField(primary_key=True, blank=False)
+    user = models.ForeignKey(User, related_name='paciente_medico', on_delete=models.CASCADE)
     nombre = models.CharField('Nombre',max_length=40)
     apellido = models.CharField('Apellido',max_length=40, null= True)
     telefono = models.IntegerField (default=0)
     genero = models.CharField(max_length=1, help_text='Seleccione el genero )')
     especialidad = models.CharField(max_length=100)
     registro = models.CharField(max_length=100)
-    user = models.ForeignKey(User, related_name='paciente_medico', on_delete=models.CASCADE)
