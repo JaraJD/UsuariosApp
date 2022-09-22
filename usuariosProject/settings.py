fe9244ffcd4ef76d95dc45ee0ddc6b7cea90212e
JuanJaramilloSalazar <salazarjdj@gmail.com>
2022-09-22 14:15:13 -0500

    Configuracion de CORS

--- usuariosProject/settings.py
+++ usuariosProject/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = 'django-insecure-hfv9jtqhc8dgxhuu%9!wi#p*flef9jh(e8ft*dchr50#k_qek&
 DEBUG = True
 
 ALLOWED_HOSTS = []
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Application definition
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'usuariosApp',
+    'corsheaders',
 ]
 
 SIMPLE_JWT = {
@@ -61,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 REST_FRAMEWORK = {
